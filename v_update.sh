2e638ee5caf0433d37aeb0de49e7f03a2d4be7d6
Karan Shukla <karan.shukla@wingify.com>
2023-08-19 23:26:07 +0530

    token changed

--- v_update.sh
+++ v_update.sh
@@ -38,7 +38,7 @@ else
         echo "package.json updated and changes pushed to branch '$NEW_BRANCH_NAME'."
 
         # # Create pull request
-        GITHUB_TOKEN="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"
+        GITHUB_TOKEN="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"
 
         # Set pull request title and description
         PR_TITLE="For Updating Version $DATE_TIME"
